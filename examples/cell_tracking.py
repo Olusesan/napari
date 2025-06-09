9f321e87896a49ceaa62701b2872457bb52c0a11
Olusesan <olusesan.ajina@gmail.com>
2025-06-08 17:49:32 -0700

    add napari

--- examples/cell_tracking.py
+++ examples/cell_tracking.py
@@ -10,15 +10,19 @@ Disease Research (CIID), University Hospital Heidelberg, Germany for the data.
 You can find the data on: https://doi.org/10.5281/zenodo.15597019
 
 """
+import glob
 import os
+import zipfile
 
 import pooch
+
+import napari
 
 ###############################################################################
 # Download the data
 # ------------------
 download = pooch.DOIDownloader(progressbar=True)
-doi = '10.5281/zenodo.15597019'
+doi = "10.5281/zenodo.15597019"
 tmp_dir = pooch.os_cache('napari-cell-tracking-example')
 os.makedirs(tmp_dir, exist_ok=True)
 
@@ -28,6 +32,22 @@ data_path = tmp_dir/data_files
 
 if not data_path.exists():
     print(f"downloading archive {data_files}")
-    download(f"doi: {doi}/{data_files}", output_file=data_path, pooch=None)
+    #download(f"doi:{doi}", output_file=data_path, path=data_files)
+    download(f"doi:{doi}", output_file=data_path, pooch=None)
 else:
     print(f"using cached {data_path}")
+
+with zipfile.ZipFile(data_path, 'r') as zip_ref:
+    zip_ref.extractall(tmp_dir)
+
+imgs = glob.glob(os.path.join(tmp_dir, "*.tif"))
+
+if not imgs:
+    raise FileNotFoundError("no tiff files found")
+
+viewer = napari.Viewer()
+
+viewer.add_image(imgs, name='Cell Tracking', colormap='gray',
+                 metadata={'Unit': 'Hours'})
+
+napari.run()
